cc505ae558d9bf9d4eabb8f52dbdaa7a5fee6b65
fabito <fuechi@ciandt.com>
2017-06-12 17:30:13 -0300

    change env variable name

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,12 @@ WORKDIR /usr/src/app
 COPY requirements.txt /usr/src/app/
 
 RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir gunicorn
 
 COPY . /usr/src/app
 
 EXPOSE 8080
 
-CMD python run.py
+ENV WORKERS=4
+
+CMD gunicorn -w ${WORKERS} -b 0.0.0.0:8080 run:app
--- kingpick_auth/config.py
+++ kingpick_auth/config.py
@@ -4,7 +4,7 @@ from .infrastructure.compute_engine_metadata import get_project_id
 JWT_PRIVATE_KEY = os.environ.get('JWT_PRIVATE_KEY', 'SET ME UP').replace('\\n', '\n')
 
 try:
-    GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
+    GCP_PROJECT_ID = os.environ['GOOGLE_CLOUD_PROJECT']
 except KeyError:  # pragma: NO COVER
     GCP_PROJECT_ID = get_project_id()
 
