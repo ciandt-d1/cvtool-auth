e9bf6dc54ad416794529c7e2dc211acb9b1de891
fabito <fuechi@ciandt.com>
2017-10-03 17:25:44 -0300

    add cors support

--- run.py
+++ run.py
@@ -4,13 +4,19 @@ import logging
 import sys
 
 import connexion
+from flask_cors import CORS
 from google.cloud.logging.handlers.container_engine import ContainerEngineHandler
 
 from kingpick_auth.encoder import JSONEncoder
 
-app = connexion.App(__name__, specification_dir='./kingpick_auth/swagger/')
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+root.addHandler(ContainerEngineHandler(sys.stdout))
+
+app = connexion.App(__name__, specification_dir='./kingpick_auth/swagger/', swagger_json=True)
 app.app.json_encoder = JSONEncoder
-app.add_api('swagger.yaml', arguments={'title': 'Provides APIs for tenant maintenance'})
+CORS(app.app)
+app.add_api('swagger.yaml', swagger_json=True, arguments={'title': 'Auth API'})
 
 
 def main():
@@ -18,11 +24,6 @@ def main():
     HOST = os.getenv('HOST', '0.0.0.0')
     PORT = os.getenv('PORT', 8080)
     DEBUG = os.getenv('DEBUG', False)
-
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    root.addHandler(ContainerEngineHandler(sys.stdout))
-
     app.run(port=PORT, debug=DEBUG, host=HOST)
 
 
